1281caca41e29ba42db0b115a5c138ff03bffc50
Hannah Song <hannahsong89@gmail.com>
2023-05-01 20:48:34 -0500

    first commit

---
+++ movie-watchlist/src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+import Header from './components/Header';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import MovieScreen from './components/MovieScreen';
+import Watchlist from './components/Watchlist';
+
+function App() {
+  const [movieList, setMovieList] = useState([]);
+  const [list, setWatchlist] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const addMovie = (movie) => setWatchlist([...list, movie]);
+  const removeMovie = (movie) => {
+    const newState = list.filter((mov) => {
+      return mov !== movie;
+    });
+    setWatchlist(newState);
+  };
+
+  const getData = () => {
+    axios
+      .get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`)
+      .then((res) => {
+        console.log(res.data.results);
+        setMovieList(res.data.results);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
+
+  return (
+    <div className="App">
+     <Header />
+     <main>
+      <MovieScreen
+      addMovie={addMovie}
+      movieList={movieList}
+      page={page}
+      setPage={setPage}
+      list={list}
+      removeMovie={removeMovie}
+      />
+    
+      <Watchlist
+      list={list}
+      removeMovie={removeMovie}
+      />
+     </main>
+    </div>
+  );
+}
+
+export default App;
